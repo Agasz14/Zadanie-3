41e4e370d8afe03ace10c194de8178294766692c
Agasz14 <agnieszka.szajewska@gmail.com>
2024-03-28 11:50:59 +0100

    zadanie 3

---
+++ fruits.js
@@ -0,0 +1,12 @@
+// fruits.js
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+console.log (fruits)
+
+// numbers.js
+const numbers = [1, 7, 8, 4, 9, 15];
+
+for (let i = 0; i < numbers.length; i += 1) (
+    if (numbers (i) > 5)  {
+        console.log(numbers[i])
+    }
+)
